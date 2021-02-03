85dac6df45cd2d343f87227bbbff33640384a2e8
zhouL <lz.mms@foxmail.com>
2021-02-03 17:19:20 +0800

    Initial commit

---
+++ app/src/main/java/com/lodz/android/eagles/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.lodz.android.eagles
+
+import com.lodz.android.pandora.base.activity.AbsActivity
+
+class MainActivity : AbsActivity() {
+    override fun getAbsLayoutId(): Int = R.layout.activity_main
+}
