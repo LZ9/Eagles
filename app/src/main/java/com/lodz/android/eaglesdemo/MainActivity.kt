d601be0d1eace7e2579b1c4928525897559ab09d
zhouL <lz.mms@foxmail.com>
2021-03-17 17:11:13 +0800

    1、增加图片选择回调逻辑；2、优化代码逻辑

--- app/src/main/java/com/lodz/android/eaglesdemo/MainActivity.kt
+++ app/src/main/java/com/lodz/android/eaglesdemo/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.lodz.android.eaglesdemo
 
+import android.content.Context
+import android.media.AudioManager
+import android.media.MediaPlayer
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 import com.lodz.android.corekt.anko.bindView
 import com.lodz.android.eagles.EaglesManager
 import com.lodz.android.pandora.base.activity.BaseActivity
@@ -15,6 +20,13 @@ class MainActivity : BaseActivity() {
     private val mPhotoBtn by bindView<Button>(R.id.photo_btn)
 
     private val mResultTv by bindView<TextView>(R.id.result_tv)
+
+    private var mMediaPlayer: MediaPlayer? = null
+
+
+    private val mLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        mResultTv.text = uri?.toString() ?: "未选择图片"
+    }
 
     override fun getLayoutId(): Int = R.layout.activity_main
 
@@ -35,18 +47,42 @@ class MainActivity : BaseActivity() {
             mResultTv.text = ""
             EaglesManager.create()
                 .setOnScanListener { isSuccess, text ->
+                    soundBeep()
                     mResultTv.text = "isSuccess : $isSuccess \n $text"
                 }
                 .scan(getContext())
         }
 
         mPhotoBtn.setOnClickListener {
-
+            mLauncher.launch("image/*")
         }
     }
 
     override fun initData() {
         super.initData()
+        initMediaPlayer()
         showStatusCompleted()
+    }
+
+    private fun initMediaPlayer(){
+        val audioManager = getContext().getSystemService(Context.AUDIO_SERVICE) as AudioManager
+        val volume = audioManager.getStreamVolume(AudioManager.STREAM_NOTIFICATION)
+        if (volume > 0){
+            mMediaPlayer = MediaPlayer.create(getContext(), R.raw.beep)
+        }
+    }
+
+    private fun soundBeep(){
+        mMediaPlayer?.start()
+    }
+
+    private fun releaseMediaPlayer(){
+        mMediaPlayer?.release()
+        mMediaPlayer = null
+    }
+
+    override fun finish() {
+        super.finish()
+        releaseMediaPlayer()
     }
 }
--- eagles/src/main/java/com/rexnjc/ui/tool/ToolsCaptureActivity.java
+++ eagles/src/main/java/com/rexnjc/ui/tool/ToolsCaptureActivity.java
@@ -1,40 +1,37 @@
 
 package com.rexnjc.ui.tool;
 
-import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Matrix;
 import android.graphics.Rect;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.WindowManager;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.PermissionChecker;
-
 import com.alipay.mobile.bqcscanservice.BQCScanCallback;
-import com.alipay.mobile.bqcscanservice.BQCScanEngine;
 import com.alipay.mobile.bqcscanservice.BQCScanError;
 import com.alipay.mobile.bqcscanservice.BQCScanService;
 import com.alipay.mobile.bqcscanservice.CameraHandler;
 import com.alipay.mobile.bqcscanservice.impl.MPaasScanServiceImpl;
 import com.alipay.mobile.mascanengine.IOnMaSDKDecodeInfo;
 import com.alipay.mobile.mascanengine.MaScanCallback;
+import com.alipay.mobile.mascanengine.MaScanEngineService;
 import com.alipay.mobile.mascanengine.MultiMaScanResult;
+import com.alipay.mobile.mascanengine.impl.MaScanEngineServiceImpl;
+import com.lodz.android.eagles.EaglesManager;
 import com.lodz.android.eagles.R;
-import com.rexnjc.ui.camera.ScanHandler;
-import com.rexnjc.ui.util.AutoZoomOperator;
+import com.lodz.android.eagles.ScanUiHandler;
+import com.lodz.android.eagles.event.EaglesScanEvent;
 import com.rexnjc.ui.widget.APTextureView;
 import com.rexnjc.ui.widget.ma.ToolScanTopView;
 
-public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanResultCallbackProducer{
+import org.greenrobot.eventbus.EventBus;
+
+public class ToolsCaptureActivity extends Activity {
 
     public static void start(Context context) {
         Intent starter = new Intent(context, ToolsCaptureActivity.class);
@@ -47,9 +44,7 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
     private BQCScanService bqcScanService;
     private boolean bqcServiceSetup;
     private CameraHandler cameraScanHandler;
-    private ScanHandler scanHandler;
     private boolean firstAutoStarted = false;
-    private boolean isPermissionGranted = false;
 
     // 是否已经摄像头扫码成功
     private int pauseOrResume = 0;
@@ -57,30 +52,21 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
     // 裁剪区域改为局部变量，避免onResume后的再次计算
     private Rect scanRect;
     private long postcode = -1;
-    private AutoZoomOperator autoZoomOperator;
+//    private AutoZoomOperator autoZoomOperator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan);
-        autoZoomOperator = new AutoZoomOperator(this);
+//        autoZoomOperator = new AutoZoomOperator(this);
         bqcScanService = new MPaasScanServiceImpl();
         bqcScanService.serviceInit();
         cameraScanHandler = bqcScanService.getCameraHandler();
-        scanHandler = new ScanHandler();
-        scanHandler.setBqcScanService(bqcScanService);
-        String permission = Manifest.permission.CAMERA;
-        if(PermissionChecker.checkSelfPermission(this, permission)
-                != PermissionChecker.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{permission}, 1);
-        } else {
-            isPermissionGranted = true;
-            firstAutoStarted = true;
-            try {
-                autoStartScan();
-            } catch (Exception ex) {
-                Log.e(TAG, "autoStartScan: Exception " + ex.getMessage());
-            }
+        firstAutoStarted = true;
+        try {
+            autoStartScan();
+        } catch (Exception ex) {
+            Log.e(TAG, "autoStartScan: Exception " + ex.getMessage());
         }
         initViews();
     }
@@ -100,108 +86,84 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
         if (mSurfaceView != null && bqcServiceSetup) {
             bqcScanService.setDisplay(mSurfaceView);
             cameraScanHandler.onSurfaceViewAvailable();
-            if (scanHandler == null) {
-                scanHandler = new ScanHandler();
-                scanHandler.setBqcScanService(bqcScanService);
-            }
-            scanHandler.registerAllEngine(false);
+            ScanUiHandler.post(new Runnable() {
+                @Override
+                public void run() {
+                    MaScanEngineService maScanEngineService = new MaScanEngineServiceImpl();
+                    bqcScanService.regScanEngine(EaglesManager.TAG, maScanEngineService.getEngineClazz(), new MaScanCallbackWithDecodeInfoSupport() {
+                        @Override
+                        public void onResultMa(final MultiMaScanResult maScanResult) {
+                            scanSuccess = true;
+                            ScanUiHandler.post(new Runnable() {
+                                @Override
+                                public void run() {
+                                    bqcScanService.setScanEnable(false);
+                                    //scanHandler.shootSound();
+                                }
+                            });
+//                    onResultShow();
+//                    if (mScanTopView != null) {
+//                        mScanTopView.onResultMa(maScanResult);
+//                    }
+                            if (maScanResult == null) {
+//                        Toast.makeText(getContext().getApplicationContext(), "error:maScanResult ==null", Toast.LENGTH_LONG).show();
+                                Log.e("testtag", "error:maScanResult ==null");
+                                restartScan();
+                                return;
+                            }
+                            Log.d("testtag", "onResultMa: " + maScanResult.maScanResults[0].text);
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    EventBus.getDefault().post(new EaglesScanEvent(true, maScanResult.maScanResults[0].text));
+                                    finish();
+                                }
+                            });
+//                    scanHandler.enableScan();
+                        }
+
+                        @Override
+                        public void onGetMaProportion(float v) {
+                            if (null != mScanTopView) {
+                                mScanTopView.onGetMaProportion(v);
+                            }
+                        }
+
+                        @Override
+                        public void onGetAvgGray(int i) {
+                            if (null != mScanTopView) {
+                                mScanTopView.onGetAvgGray(i);
+                            }
+                        }
+                    });
+                }
+            });
             setScanType(true);
         }
     }
 
 
-    public void setScanType( boolean firstAutoStarted) {
-        if (!firstAutoStarted  || bqcScanService == null) {
+    public void setScanType(boolean firstAutoStarted) {
+        if (!firstAutoStarted || bqcScanService == null) {
             return;
         }
-        scanHandler.disableScan();
-        scanHandler.setScanType();
-        scanHandler.enableScan();
+        ScanUiHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                bqcScanService.setScanEnable(false);
+                bqcScanService.setScanType(EaglesManager.TAG);
+                bqcScanService.setScanEnable(true);
+            }
+        });
     }
 
     private void autoStartScan() {
         // 初始时都为关闭状态
-        cameraScanHandler.init(this, bqcCallback);
-        scanHandler.setContext(this, this);
+        cameraScanHandler.init(this, mBQCScanCallback);
         startPreview();
     }
 
-    void showPermissionDenied() {
-        if (!this.isFinishing()) {
-            showAlertDialog(getString(R.string.camera_no_permission));
-        }
-    }
-
     interface MaScanCallbackWithDecodeInfoSupport extends MaScanCallback, IOnMaSDKDecodeInfo {
-    }
-
-    @Override
-    public BQCScanEngine.EngineCallback makeScanResultCallback() {
-        MaScanCallbackWithDecodeInfoSupport maCallback = new MaScanCallbackWithDecodeInfoSupport() {
-            @Override
-            public void onResultMa(final MultiMaScanResult maScanResult) {
-                scanSuccess = true;
-                if(scanHandler != null) {
-                    scanHandler.disableScan();
-                    scanHandler.shootSound();
-                }
-//                    onResultShow();
-//                    if (mScanTopView != null) {
-//                        mScanTopView.onResultMa(maScanResult);
-//                    }
-                if (maScanResult == null) {
-//                        Toast.makeText(getContext().getApplicationContext(), "error:maScanResult ==null", Toast.LENGTH_LONG).show();
-                    Log.e("testtag", "error:maScanResult ==null");
-                    restartScan();
-                    return;
-                }
-                Log.d("testtag", "onResultMa: " + maScanResult.maScanResults[0].text);
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        showAlertDialog(maScanResult.maScanResults[0].text);
-                    }
-                });
-//                    scanHandler.enableScan();
-            }
-
-            @Override
-            public void onGetMaProportion(float v) {
-                if (null != mScanTopView) {
-                    mScanTopView.onGetMaProportion(v);
-                }
-            }
-
-            @Override
-            public void onGetAvgGray(int i) {
-                if (null != mScanTopView) {
-                    mScanTopView.onGetAvgGray(i);
-                }
-            }
-        };
-        return maCallback;
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode == 1 && permissions != null && grantResults != null) {
-            for(int i = 0; i < permissions.length && i < grantResults.length; ++i) {
-                if(TextUtils.equals(permissions[i],Manifest.permission.CAMERA)) {
-                    if(grantResults[i] != PackageManager.PERMISSION_GRANTED ) {
-                        showPermissionDenied();
-                        break;
-                    } else {
-                        firstAutoStarted = true;
-                        try {
-                            autoStartScan();
-                        } catch (Exception ex) {
-                            Log.e(TAG, "autoStartScan: Exception " + ex.getMessage());
-                        }
-                    }
-                }
-            }
-        }
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 
     public void startPreview() {
@@ -212,14 +174,22 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
 
     private void realStopPreview() {
         cameraScanHandler.closeCamera();
-        scanHandler.disableScan();
+        ScanUiHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                bqcScanService.setScanEnable(false);
+            }
+        });
     }
 
     private void restartScan() {
-        if(scanHandler != null) {
-            scanHandler.enableScan();
-            scanSuccess = false;
-        }
+        ScanUiHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                bqcScanService.setScanEnable(true);
+                scanSuccess = false;
+            }
+        });
     }
 
     private void initScanRect() {
@@ -229,7 +199,7 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
             float screenWith = wm.getDefaultDisplay().getWidth();
             float screenHeight = wm.getDefaultDisplay().getHeight();
             scanRect = new Rect(0, 0, wm.getDefaultDisplay().getWidth(), wm.getDefaultDisplay().getHeight());
-            float previewScale  = 1.0f;
+            float previewScale = 1.0f;
 //            float previewScale = screenWith / cropWidth;
 //            if (previewScale < 1.0f) {
 //                previewScale = 1.0f;
@@ -239,7 +209,7 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
 //            }
             Log.d(TAG, "previewScale: " + previewScale);
             Matrix transform = new Matrix();
-            transform.setScale(previewScale, previewScale, screenWith , screenHeight );
+            transform.setScale(previewScale, previewScale, screenWith, screenHeight);
             mSurfaceView.setTransform(transform);
             bqcScanService.setScanRegion(scanRect);
         } else {
@@ -251,18 +221,11 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
     public void onResume() {
         super.onResume();
         pauseOrResume = 1;
-        if (scanHandler == null) {
-            scanHandler = new ScanHandler();
-            scanHandler.setBqcScanService(bqcScanService);
-        }
-        if (!firstAutoStarted && !scanSuccess
-                && mScanTopView != null) {
-            if (isPermissionGranted) {
-                try {
-                    autoStartScan();
-                } catch (Exception ex) {
-                    Log.e(TAG, "autoStartScan: Exception " + ex.getMessage());
-                }
+        if (!firstAutoStarted && !scanSuccess && mScanTopView != null) {
+            try {
+                autoStartScan();
+            } catch (Exception ex) {
+                Log.e(TAG, "autoStartScan: Exception " + ex.getMessage());
             }
         }
     }
@@ -272,50 +235,42 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
         super.onPause();
         pauseOrResume = -1;
         firstAutoStarted = false;
-        if (isPermissionGranted) {
-            realStopPreview();
-        }
+        realStopPreview();
         if (bqcScanService != null && cameraScanHandler != null) {
             cameraScanHandler.release(postcode);
-        }
-        if(scanHandler != null) {
-            scanHandler.reset();
         }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if(bqcScanService != null) {
+        if (bqcScanService != null) {
             bqcScanService.serviceOut();
         }
-        if (scanHandler != null) {
-            scanHandler.removeContext();
-            scanHandler.destroy();
-        }
-        if(mScanTopView != null) {
+        if (mScanTopView != null) {
             mScanTopView.detachActivity();
         }
-        if(autoZoomOperator != null) {
-            autoZoomOperator.clearActivity();
-        }
+//        if (autoZoomOperator != null) {
+//            autoZoomOperator.clearActivity();
+//        }
     }
 
     private void showAlertDialog(String msg) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(msg)
                 .setPositiveButton(getString(R.string.ok), new DialogInterface.OnClickListener() {
-            @Override public void onClick(DialogInterface dialog, int which) {
-                finish();
-            }
-        }).create().show();
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).create().show();
     }
 
-    private BQCScanCallback bqcCallback = new BQCScanCallback() {
+    private BQCScanCallback mBQCScanCallback = new BQCScanCallback() {
 
         @Override
         public void onParametersSetted(final long pcode) {
-            if(!isFinishing()) {
+            if (!isFinishing()) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
@@ -385,10 +340,13 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
         public void onOuterEnvDetected(boolean shouldShow) {
         }
 
-        @Override public void onCameraReady() {
+        @Override
+        public void onCameraReady() {
 
         }
-        @Override public void onCameraClose() {
+
+        @Override
+        public void onCameraClose() {
 
         }
     };
@@ -408,10 +366,6 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
         @Override
         public void startPreview() {
             // 选取照片识别后, 会走客户端的BaseFragment的onPause流程,此时CameraManager被cleanup,
-            if (scanHandler == null) {
-                scanHandler = new ScanHandler();
-                scanHandler.setBqcScanService(bqcScanService);
-            }
             if (bqcScanService != null && bqcScanService.getCamera() == null) {
                 autoStartScan();
             }
@@ -441,9 +395,9 @@ public class ToolsCaptureActivity extends Activity implements ScanHandler.ScanRe
     }
 
     public void startContinueZoom(int zoom) {
-        if(autoZoomOperator != null) {
-            autoZoomOperator.startAutoZoom(zoom, 0);
-        }
+//        if (autoZoomOperator != null) {
+//            autoZoomOperator.startAutoZoom(zoom, 0);
+//        }
     }
 
     public void setZoom(int zoom) {
